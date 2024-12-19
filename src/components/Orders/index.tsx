f17bab02b4b62915ba1cea1a3e8c264f35814c3b
oliveiradniel <kadadniel@gmail.com>
2024-12-19 10:18:16 -0300

    feat(Orders): create and style the 'Board' component

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,9 +1,26 @@
-import { Container } from './styles';
+import { Board, Container, OrdersContainer } from './styles';
 
 export default function Orders() {
   return (
     <Container>
-      Orders
+      <Board>
+        <header>
+          <span>🕛</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 Items</span>
+          </button>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 Items</span>
+          </button>
+        </OrdersContainer>
+      </Board>
     </Container>
   );
 }
--- src/components/Orders/styles.ts
+++ src/components/Orders/styles.ts
@@ -5,3 +5,49 @@ export const Container = styled.div`
   max-width: 1216px;
   width: 100%;
 `;
+
+export const  Board = styled.div`
+  border: 1px solid rgba(204, 204, 204, 0.4);
+  border-radius: 16px;
+  padding: 16px;
+
+  header {
+    align-items: center;
+    display: flex;
+    font-size: 14px;
+    gap: 8px;
+    justify-content: center;
+    padding: 8px;
+  }
+`;
+
+export const OrdersContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 24px;
+
+  button {
+    align-items: center;
+    background-color: #fff;
+    border: 1px solid rgba(204, 204, 204, 0.4);
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+    height: 128px;
+    justify-content: center;
+
+    & + button {
+      margin-top: 24px;
+    }
+
+    strong {
+      font-weight: 500;
+    }
+
+    span {
+      font-size: 14px;
+      color: #666;
+    }
+  }
+`;
